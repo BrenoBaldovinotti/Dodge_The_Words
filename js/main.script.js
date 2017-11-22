99018b0b3994ca15c2febff12dc2ae2ab76c7e91
Breno Baldovinotti <breno.baldovinotti@mblabs.com.br>
2017-11-22 01:51:12 -0200

    fixing merge problems

--- js/main.script.js
+++ js/main.script.js
@@ -1,15 +1,5 @@
 
-var palavras = ["Quatro",
-"Urso",
-"Universidade",
-"Forquilha",
-"Insurreição",
-"Paralelepípedo",
-"Maçã",
-"Enfeitiçar",
-"Úlcera",
-"Balde",
-"Pólvora",
+var palavras = ["Quatro","Urso","Universidade","Forquilha","Insurreição","Paralelepípedo","Maçã","Enfeitiçar","Úlcera","Balde","Pólvora",
 "Irrigar",
 "Microfone",
 "Acordeão",
@@ -81,7 +71,8 @@ var palavras = ["Quatro",
 "Percussão"];
 
 
-function GerarPalavra() {
+window.onload = function GerarPalavra(wordId) {
+
 $('#PalavraCima').html(palavras[GerarNumeroAleatorio(palavras.length - 1)]);
 $('#PalavraMeio').html(palavras[GerarNumeroAleatorio(palavras.length - 1)]);
 $('#PalavraBaixo').html(palavras[GerarNumeroAleatorio(palavras.length - 1)]);
@@ -108,6 +99,7 @@ function RandomWordComplete(data) {
 console.log(data.Word);
 }
 
+RandomWord();
 
 /*mantem sempre o foco no input*/
 $(document).ready(function() {
@@ -177,204 +169,207 @@ function updateScore () {
 	$("#score-result").html(" " + score);
 }
 
+
 var opcoes = null;
 
 function init_options()
 {
-	
-	var retrievedObject = localStorage.getItem('opcoes');	
-	
 
-	console.log("valor " + retrievedObject);
-	console.log('retrievedObject: ', JSON.parse(retrievedObject));
-	
-	if (retrievedObject != "null") {
-		
-		opcoes = JSON.parse(retrievedObject);
+    var retrievedObject = localStorage.getItem('opcoes');
 
-	} else {
-		console.log("NULL!!! ");
-		opcoes = {
-			velocidade:15, 
-			dificuldade:"Facil", 
-			multiplicadorDificuldade:1,	
-			frequencia:5,
-		};
-		localStorage.setItem('opcoes', JSON.stringify(opcoes));		
-	}
+
+    console.log("valor " + retrievedObject);
+    console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+    if (retrievedObject != null) {
+        console.log("not_null");
+
+        opcoes = JSON.parse(retrievedObject);
+
+    } else {
+        console.log("NULL!!! ");
+        opcoes = {
+            velocidade:15, 
+            dificuldade:"Facil", 
+            multiplicadorDificuldade:1,
+            frequencia:5,
+        };
+        localStorage.setItem('opcoes', JSON.stringify(opcoes));
+    }
 }
 
 function getOpcoes() {
 
-	localStorage.setItem('opcoes', JSON.stringify(opcoes));		
-	
-	
+    localStorage.setItem('opcoes', JSON.stringify(opcoes));
+
+
 }
 
 function velocidade_facil() {
-	
-	opcoes.velocidade = 15;
-	highlight_velocidade();
-	getOpcoes();
+
+    opcoes.velocidade = 15;
+    highlight_velocidade();
+    getOpcoes();
 }
 
 function velocidade_medio() {
-	
-	opcoes.velocidade = 10;
-	highlight_velocidade();
-	getOpcoes();
+
+    opcoes.velocidade = 10;
+    highlight_velocidade();
+    getOpcoes();
 }
 
 function velocidade_dificil() {
-	
-	opcoes.velocidade = 5;
-	highlight_velocidade();
-	getOpcoes();
+
+    opcoes.velocidade = 5;
+    highlight_velocidade();
+    getOpcoes();
 }
 
 function dificuldade_facil() {
-	
-	opcoes.dificuldade = "Facil";
-	opcoes.multiplicadorDificuldade = 1;
-	highlight_dificuldade();
-	
-	
-	getOpcoes();
+
+    opcoes.dificuldade = "Facil";
+    opcoes.multiplicadorDificuldade = 1;
+    highlight_dificuldade();
+
+
+    getOpcoes();
 }
 
 function dificuldade_medio() {
-	
-	opcoes.dificuldade = "Medio";
-	opcoes.multiplicadorDificuldade = 2;	
-	highlight_dificuldade();
-	
-	getOpcoes();
+
+    opcoes.dificuldade = "Medio";
+    opcoes.multiplicadorDificuldade = 2;
+    highlight_dificuldade();
+
+    getOpcoes();
 }
 
 function dificuldade_dificil() {
-	
-	opcoes.dificuldade = "Dificil";
-	opcoes.multiplicadorDificuldade = 3;
-	highlight_dificuldade();
-	
-	getOpcoes();
+
+    opcoes.dificuldade = "Dificil";
+    opcoes.multiplicadorDificuldade = 3;
+    highlight_dificuldade();
+
+    getOpcoes();
 }
 
 function frequencia_facil() {
-	
-	opcoes.frequencia = 5;
-	highlight_frequencia();
-	getOpcoes();
+
+    opcoes.frequencia = 5;
+    highlight_frequencia();
+    getOpcoes();
 }
 
 function frequencia_medio() {
-	
-	opcoes.frequencia = 10;
-	highlight_frequencia();	
-	getOpcoes();
+
+    opcoes.frequencia = 10;
+    highlight_frequencia();
+    getOpcoes();
 }
 
 function frequencia_dificil() {
-	
-	opcoes.frequencia = 15;
-	highlight_frequencia();	
-	getOpcoes();
+
+    opcoes.frequencia = 15;
+    highlight_frequencia();
+    getOpcoes();
 }
 
 function highlight_dificuldade() {
-	
-	var array_values = Object.values(opcoes);
-	switch (array_values[1]) {
-		case "Facil":
-			$('#dificuldade_facil').css('color', 'red')
-			$('#dificuldade_medio').css('color', '#fff')
-			$('#dificuldade_dificil').css('color', '#fff')
-			
-		break;
-	
-		case "Medio":
-			$('#dificuldade_facil').css('color', '#fff')		
-			$('#dificuldade_medio').css('color', 'red')
-			$('#dificuldade_dificil').css('color', '#fff')
-			
-		break;
 
-		case "Dificil":
-			$('#dificuldade_facil').css('color', '#fff')		
-			$('#dificuldade_medio').css('color', '#fff')
-			$('#dificuldade_dificil').css('color', 'red')
-		
-		break;
+    var array_values = Object.values(opcoes);
+    switch (array_values[1]) {
+        case "Facil":
+            $('#dificuldade_facil').css('color', 'red')
+            $('#dificuldade_medio').css('color', '#fff')
+            $('#dificuldade_dificil').css('color', '#fff')
 
-		default:
-			break;
-	}
+        break;
+
+        case "Medio":
+            $('#dificuldade_facil').css('color', '#fff')
+            $('#dificuldade_medio').css('color', 'red')
+            $('#dificuldade_dificil').css('color', '#fff')
+
+        break;
+
+        case "Dificil":
+            $('#dificuldade_facil').css('color', '#fff')
+            $('#dificuldade_medio').css('color', '#fff')
+            $('#dificuldade_dificil').css('color', 'red')
+
+        break;
+
+        default:
+            break;
+    }
 }
 
 function highlight_velocidade() {
-	
-	var array_values = Object.values(opcoes);
-	switch (array_values[0]) {
-		case 15:
-			$('#velocidade_facil').css('color', 'red')
-			$('#velocidade_medio').css('color', '#fff')
-			$('#velocidade_dificil').css('color', '#fff')
-			
-		break;
-	
-		case 10:
-			$('#velocidade_facil').css('color', '#fff')		
-			$('#velocidade_medio').css('color', 'red')
-			$('#velocidade_dificil').css('color', '#fff')
-			
-		break;
 
-		case 5:
-			$('#velocidade_facil').css('color', '#fff')		
-			$('#velocidade_medio').css('color', '#fff')
-			$('#velocidade_dificil').css('color', 'red')
-		
-		break;
+    var array_values = Object.values(opcoes);
+    switch (array_values[0]) {
+        case 15:
+            $('#velocidade_facil').css('color', 'red')
+            $('#velocidade_medio').css('color', '#fff')
+            $('#velocidade_dificil').css('color', '#fff')
 
-		default:
-			break;
-	}
+        break;
+
+        case 10:
+            $('#velocidade_facil').css('color', '#fff')
+            $('#velocidade_medio').css('color', 'red')
+            $('#velocidade_dificil').css('color', '#fff')
+
+        break;
+
+        case 5:
+            $('#velocidade_facil').css('color', '#fff')
+            $('#velocidade_medio').css('color', '#fff')
+            $('#velocidade_dificil').css('color', 'red')
+
+        break;
+
+        default:
+            break;
+    }
 }
+
 function highlight_frequencia() {
-	
-	var array_values = Object.values(opcoes);
-	switch (array_values[3]) {
-		case 5:
-			$('#frequencia_facil').css('color', 'red')
-			$('#frequencia_medio').css('color', '#fff')
-			$('#frequencia_dificil').css('color', '#fff')
-			
-		break;
-	
-		case 10:
-			$('#frequencia_facil').css('color', '#fff')		
-			$('#frequencia_medio').css('color', 'red')
-			$('#frequencia_dificil').css('color', '#fff')
-			
-		break;
 
-		case 15:
-			$('#frequencia_facil').css('color', '#fff')		
-			$('#frequencia_medio').css('color', '#fff')
-			$('#frequencia_dificil').css('color', 'red')
-		
-		break;
+    var array_values = Object.values(opcoes);
+    switch (array_values[3]) {
+        case 5:
+            $('#frequencia_facil').css('color', 'red')
+            $('#frequencia_medio').css('color', '#fff')
+            $('#frequencia_dificil').css('color', '#fff')
 
-		default:
-			break;
-	}
+        break;
+
+        case 10:
+            $('#frequencia_facil').css('color', '#fff')
+            $('#frequencia_medio').css('color', 'red')
+            $('#frequencia_dificil').css('color', '#fff')
+
+        break;
+
+        case 15:
+            $('#frequencia_facil').css('color', '#fff')
+            $('#frequencia_medio').css('color', '#fff')
+            $('#frequencia_dificil').css('color', 'red')
+
+        break;
+
+        default:
+            break;
+    }
 
 }
 
 function set_options()
 {
-	init_options();
-	highlight_dificuldade();
-	highlight_velocidade();
-	highlight_frequencia();
+    init_options();
+    highlight_dificuldade();
+    highlight_velocidade();
+    highlight_frequencia();
 }
