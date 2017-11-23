4639d95becbd60260c1ff85260d3e3fbe1334ec6
Breno Baldovinotti <breno.baldovinotti@mblabs.com.br>
2017-11-22 22:08:59 -0200

    added running sprite

---
+++ sprite_js.js
@@ -0,0 +1,112 @@
+ (function() {
+
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+(function () {
+			
+	var coin,
+		coinImage,
+		canvas;					
+
+	function gameLoop () {
+	
+	  window.requestAnimationFrame(gameLoop);
+
+	  coin.update();
+	  coin.render();
+	}
+	
+	function sprite (options) {
+	
+		var that = {},
+			frameIndex = 0,
+			tickCount = 0,
+			ticksPerFrame = options.ticksPerFrame || 0,
+			numberOfFrames = options.numberOfFrames || 1;
+		
+		that.context = options.context;
+		that.width = options.width;
+		that.height = options.height;
+		that.image = options.image;
+		
+		that.update = function () {
+
+            tickCount += 1;
+
+            if (tickCount > ticksPerFrame) {
+
+				tickCount = 0;
+				
+                if (frameIndex < numberOfFrames - 1) {	
+                    frameIndex += 1;
+                } else {
+                    frameIndex = 0;
+                }
+            }
+        };
+		
+		that.render = function () {
+		
+		  that.context.clearRect(0, 0, that.width, that.height);
+		  
+		  that.context.drawImage(
+		    that.image,
+		    frameIndex * that.width / numberOfFrames,
+		    0,
+		    that.width / numberOfFrames,
+		    that.height,
+		    0,
+		    0,
+		    that.width / numberOfFrames,
+		    that.height);
+		};
+		
+		return that;
+	}
+	
+	// Get canvas
+	canvas = document.getElementById("coinAnimation");
+	canvas.width = 500;
+	canvas.height = 500;
+	
+	// Create sprite sheet
+	coinImage = new Image();	
+	
+	// Create sprite
+	coin = sprite({
+		context: canvas.getContext("2d"),
+		width: 4240,
+		height: 507,
+		image: coinImage,
+		numberOfFrames: 10,
+		ticksPerFrame: 6
+	});
+	
+	// Load sprite sheet
+	coinImage.addEventListener("load", gameLoop);
+	coinImage.src = "images/Sprite_Run.png";
+
+} ());
+
